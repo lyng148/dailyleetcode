85a294948392bf8a71fe8f1e3cb0126ace7e9ed9
lyng148 <113703761+lyng148@users.noreply.github.com>
2023-09-20 15:49:12 +0700

    Time: 5 ms (50.96%) | Memory: 6.8 MB (9.06%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.c
@@ -0,0 +1,29 @@
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
+    int index = nums1Size - 1;
+    int i = m - 1;
+    int j = n - 1;
+    if (i < 0)
+    {
+        for (int k = 0; k < n; k++)
+        {
+            nums1[k] = nums2[k];
+        }
+        return;
+    }
+    if (j < 0)
+    {
+        return;
+    }
+
+    while (i >= 0 && j >= 0)
+    {
+        if (nums1[i] > nums2[j])
+        {
+            nums1[index--] = nums1[i--];
+        }
+        else nums1[index--] = nums2[j--];
+    }
+    while (j >= 0) nums1[index--] = nums2[j--]; 
+    while (i >= 0) nums1[index--] = nums1[i--];
+    return; 
+}
