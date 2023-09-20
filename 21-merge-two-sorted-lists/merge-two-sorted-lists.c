251a22580a382d765a29ea062733e5848aa5d02c
lyng148 <113703761+lyng148@users.noreply.github.com>
2023-09-21 00:58:48 +0700

    Time: 0 ms (100.00%) | Memory: 7.1 MB (6.13%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.c
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+typedef struct ListNode node;
+
+node *makenode(int val)
+{
+    node* newnode = (node*)malloc(sizeof(node));
+    newnode->val = val;
+    newnode->next = NULL;
+    return newnode;
+}
+
+struct ListNode* mergeTwoLists(struct ListNode* head1, struct ListNode* head2) {
+    node *head = NULL;
+    node *tmp = NULL;
+    
+    while (head1 != NULL && head2 != NULL) {
+        if (head1->val <= head2->val) {
+            node* next = makenode(head1->val);
+            if (tmp == NULL) {
+                head = next;
+                tmp = head;
+            } else {
+                tmp->next = next;
+                tmp = tmp->next;
+            }
+            head1 = head1->next;
+        } else {
+            node* next = makenode(head2->val);
+            if (tmp == NULL) {
+                head = next;
+                tmp = head;
+            } else {
+                tmp->next = next;
+                tmp = tmp->next;
+            }
+            head2 = head2->next;
+        }
+    }
+
+    while (head1 != NULL) {
+        node* next = makenode(head1->val);
+        if (tmp == NULL) {
+            head = next;
+            tmp = head;
+        } else {
+            tmp->next = next;
+            tmp = tmp->next;
+        }
+        head1 = head1->next;
+    }
+
+    while (head2 != NULL) {
+        node* next = makenode(head2->val);
+        if (tmp == NULL) {
+            head = next;
+            tmp = head;
+        } else {
+            tmp->next = next;
+            tmp = tmp->next;
+        }
+        head2 = head2->next;
+    }
+
+    return head;
+}
