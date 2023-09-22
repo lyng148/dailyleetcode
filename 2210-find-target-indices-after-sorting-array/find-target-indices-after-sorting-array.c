f214376ee88dd9f7a2833afb18a3097fd70e7251
lyng148 <113703761+lyng148@users.noreply.github.com>
2023-09-22 12:38:25 +0700

    Time: 3 ms (90.54%) | Memory: 6.6 MB (8.11%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.c
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.c
@@ -4,15 +4,30 @@ int cmpfunc(const void* a, const void* b) {
 
 int* targetIndices(int* nums, int numsSize, int target, int* returnSize) {
     qsort(nums, numsSize, sizeof(int), cmpfunc);
+    int l = 0, r = numsSize - 1;
     int* ans = (int*)malloc(numsSize * sizeof(int));
     int count = 0;
-
-    for (int i = 0; i < numsSize; i++) {
-        if (nums[i] == target) {
-            ans[count++] = i;
+    int index = 0;
+    
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+        if (nums[m] == target) {
+            index = m;
+            r = m - 1;
+        } else if (nums[m] < target) {
+            l = m + 1;
+        } else {
+            r = m - 1;
         }
     }
 
+    for (int i = index; i < numsSize; i++)
+    {
+        if (nums[i] == target)
+        {
+            ans[count++] = i;
+        }
+    }
     *returnSize = count;
     return ans;
 }
