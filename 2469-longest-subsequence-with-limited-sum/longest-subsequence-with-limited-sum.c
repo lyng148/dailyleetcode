b2fc71f08ee614883d3937e1cd15253b7b78176a
lyng148 <113703761+lyng148@users.noreply.github.com>
2023-09-24 15:15:59 +0700

    Time: 26 ms (60.38%) | Memory: 8.7 MB (20.75%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.c
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.c
@@ -6,7 +6,9 @@ int cmpfunc (const void * a, const void * b)
 int* answerQueries(int* nums, int numsSize, int* queries, int queriesSize, int* returnSize){
     qsort(nums, numsSize, sizeof(int), cmpfunc);
     int *ans = (int *)malloc(queriesSize * sizeof(int));
-
+    // V de tim ra chuoi dai nhat => luon dung tat ca cac so tu be den lon
+    // => lam prefix sum de tinh luon tong
+    // dung binary search tim ra gia tri <= voi gia tri can tim;
     if (numsSize > 1)
     {
         for (int i = 1; i < numsSize; i++)
@@ -17,13 +19,18 @@ int* answerQueries(int* nums, int numsSize, int* queries, int queriesSize, int* 
 
     for (int i = 0; i < queriesSize; i++)
     {
-        int index = 0;
-        while (nums[index] <= queries[i] && index < numsSize - 1)
+        int l = 0, r = numsSize - 1, k = 0;
+        while(l <= r)
         {
-            index++;
+            int m = l + (r - l) / 2;
+            if (nums[m] <= queries[i])
+            {
+                k = m + 1;
+                l = m + 1;
+            }
+            else r = m - 1;
         }
-        if (nums[numsSize - 1] <= queries[i]) index++;
-        ans[i] = index;
+        ans[i] = k;
     }
     *returnSize = queriesSize;
     return ans;
