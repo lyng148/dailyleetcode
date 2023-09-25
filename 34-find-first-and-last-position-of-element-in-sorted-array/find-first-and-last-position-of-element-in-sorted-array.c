4904a434aa25a6b3713dbe5e8f0abaa93c43d724
lyng148 <113703761+lyng148@users.noreply.github.com>
2023-09-25 08:16:45 +0700

    Time: 4 ms (87.46%) | Memory: 8.3 MB (6.86%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.c
@@ -0,0 +1,37 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* searchRange(int* nums, int numsSize, int target, int* returnSize){
+    *returnSize = 2;
+    int *ans = (int *)malloc(2 * sizeof(int));
+    int low = 0, high = numsSize - 1;
+    int k = -1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target)
+        {
+            k = mid;
+            high = mid - 1;
+        }
+        else if (nums[mid] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    ans[0] = k;
+    k = -1;
+    low = 0;
+    high = numsSize - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target)
+        {
+            k = mid;
+            low = mid + 1;
+        }
+        else if (nums[mid] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    ans[1] = k;
+    return ans;
+}
