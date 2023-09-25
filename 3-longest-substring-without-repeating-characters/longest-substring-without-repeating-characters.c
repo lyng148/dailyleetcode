9dddb65dc445bab886a9e359fb3c87ae2b38c2d8
lyng148 <113703761+lyng148@users.noreply.github.com>
2023-09-25 17:14:46 +0700

    Time: 7 ms (50.05%) | Memory: 6.8 MB (6.35%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.c
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.c
@@ -1,17 +1,17 @@
-int lengthOfLongestSubstring(char *s) {
-    if (strcmp(s, "") == 0) return 0;
+int lengthOfLongestSubstring(char * s){
+    if (strcmp(s,"") == 0) return 0;
     int max = 0;
-    int map[256] = {0}; // Sửa thành 256 vì ASCII có 256 ký tự
+    int map[256] = {0};
     int l = 0;
-    int r = 0;
-
-    while (s[r] != '\0') {
+    for (int r = 0; s[r] != '\0'; r++) {
         if (map[s[r]] == 0) {
             map[s[r]] = 1;
             if (r - l + 1 > max) max = r - l + 1;
-            r++;
         } else {
-            map[s[l]] = 0;
+            while (s[l] != s[r]) {
+                map[s[l]] = 0;
+                l++;
+            }
             l++;
         }
     }
