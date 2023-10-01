e8e79df3a849a0496e6eae66a1a7920a1ff9cdeb
lyng148 <113703761+lyng148@users.noreply.github.com>
2023-10-01 20:58:03 +0700

    Time: 8 ms (44.79%) | Memory: 7.7 MB (18.40%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.c
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.c
@@ -1,23 +1,10 @@
 
 char * reverseWords(char * s){
     int l = 0, index = 0;
-    int len = strlen(s); // Lấy chiều dài của chuỗi
+    int len = strlen(s);
     for (int i = 0; i < len; i++)
     {
-        if (s[i] == ' ')
-        {
-            l = i - 1;
-            while (index < l)
-            {
-                char tmp = s[index];
-                s[index] = s[l];
-                s[l] = tmp;
-                index++;
-                l--;
-            }
-            index = i + 1;
-        }
-        else if (i == len - 1)
+        if (s[i + 1] == ' ' || i == len - 1)
         {
             l = i;
             while (index < l)
@@ -28,8 +15,8 @@ char * reverseWords(char * s){
                 index++;
                 l--;
             }
+            index = i + 2;
         }
-        
     }
     return s;
 }
