7680a76b42b3a3355cf3760dcdf2f4b718570168
lyng148 <113703761+lyng148@users.noreply.github.com>
2023-10-06 11:09:53 +0700

    Time: 2 ms (54.29%) | Memory: 6.4 MB (11.43%) - LeetSync

---
+++ 343-integer-break/integer-break.c
@@ -0,0 +1,14 @@
+int integerBreak(int n) {
+    if (n < 2) {
+        return 0;
+    }
+    int dp[n + 1];
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        dp[i] = 0;
+        for (int j = 1; j < i; j++) {
+            dp[i] = fmax(dp[i], fmax(j * (i - j), j * dp[i - j]));
+        }
+    }
+    return dp[n];
+}
