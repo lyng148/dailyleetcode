5f3aa987154bc8a63843990dcd54becdbdad3f9f
lyng148 <113703761+lyng148@users.noreply.github.com>
2023-10-10 23:28:14 +0700

    Time: 30 ms (22.93%) | Memory: 7.5 MB (36.72%) - LeetSync

--- 792-binary-search/binary-search.c
+++ 792-binary-search/binary-search.c
@@ -1,11 +1,13 @@
-int search(int* nums, int numsSize, int target){
-    int l = 0, r = numsSize - 1, mid;
-    while (l <= r)
-    {
-        mid = l + (r - l) / 2;
-        if (nums[mid] == target) return mid;
-        else if (nums[mid] < target) l = mid + 1;
-        else r = mid - 1;
+int bssearch(int *nums, int l, int r, int target) {
+    if (l <= r) {
+        int m = l + (r - l) / 2;
+        if (nums[m] == target) return m;
+        else if (nums[m] < target) return bssearch(nums, m + 1, r, target);
+        else return bssearch(nums, l, m - 1, target);
     }
     return -1;
+}
+
+int search(int* nums, int numsSize, int target){
+    return bssearch(nums, 0, numsSize - 1, target);
 }
