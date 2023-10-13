821d33edabf395bb5ba88ecf5061580e67228962
lyng148 <113703761+lyng148@users.noreply.github.com>
2023-10-13 13:20:23 +0700

    Time: 7 ms (75.10%) | Memory: 6.5 MB (31.27%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.c
+++ 36-valid-sudoku/valid-sudoku.c
@@ -1,21 +1,21 @@
-bool check(char** board, int row, int col, char target) {
+bool check(char** board, int row, int col, int target) {
+    int colStart = col - (col % 3);
+    int rowStart = row - (row % 3);
+
+    for (int i = rowStart; i <= rowStart + 2; i++) {
+        for (int j = colStart; j <= colStart + 2; j++) {
+            if (board[i][j] == target)
+                if (i != row && j != col)
+                    return false;
+        }
+    }
+
     for (int i = 0; i < 9; i++) {
         if (i != row && board[i][col] == target)
             return false;
         if (i != col && board[row][i] == target)
             return false;
     }
-
-    int rowStart = row - row % 3;
-    int colStart = col - col % 3;
-
-    for (int i = rowStart; i < rowStart + 3; i++) {
-        for (int j = colStart; j < colStart + 3; j++) {
-            if ((i != row || j != col) && board[i][j] == target)
-                return false;
-        }
-    }
-
     return true;
 }
 
