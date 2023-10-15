d1a009c4aaac3809a14134fd28da0e62b08e3f2a
lyng148 <113703761+lyng148@users.noreply.github.com>
2023-10-15 23:09:27 +0700

    Time: 0 ms (100.00%) | Memory: 8.8 MB (45.83%) - LeetSync

--- 112-path-sum/path-sum.c
+++ 112-path-sum/path-sum.c
@@ -6,26 +6,15 @@
  *     struct TreeNode *right;
  * };
  */
-typedef struct TreeNode node;
 
+typedef struct TreeNode node;
 bool check(node* root, int target)
 {
-    bool leftCheck, rightCheck;
-
-    if (root == NULL) 
-        return false;
-
-    if (root->right == NULL && root->left == NULL) //root is a leaf
-    {
-        return root->val == target;
-    }
-
-    target -= root ->val;
-
-    leftCheck = check(root->left, target);
-    rightCheck = check(root->right, target);
-    return (leftCheck || rightCheck);
-} 
-bool hasPathSum(node* root, int targetSum){
+    if (root == NULL) return false;
+    if (root->left == NULL && root->right == NULL) return root->val == target;
+    target -= root->val;
+    return check(root->left, target) || check(root->right, target);
+}
+bool hasPathSum(struct TreeNode* root, int targetSum){
     return check(root, targetSum);
 }
