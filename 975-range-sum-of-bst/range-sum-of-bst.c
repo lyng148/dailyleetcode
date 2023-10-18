f935097652cf66b389d35df590c7332b094ffe47
lyng148 <113703761+lyng148@users.noreply.github.com>
2023-10-18 16:43:04 +0700

    Time: 76 ms (10.71%) | Memory: 43.1 MB (50.00%) - LeetSync

---
+++ 975-range-sum-of-bst/range-sum-of-bst.c
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+typedef struct TreeNode TreeNode;
+
+void DFS(TreeNode *root, int low, int high, int *ans)
+{
+    if (root == NULL) return;
+    if (low <= root->val && root->val <= high)
+    {
+        (*ans) += root->val;
+    }
+    DFS(root->left, low, high, ans);
+    DFS(root->right, low, high, ans);
+} 
+int rangeSumBST(struct TreeNode* root, int low, int high){
+    int ans = 0;
+    DFS(root, low, high, &ans);
+    return ans;
+}
