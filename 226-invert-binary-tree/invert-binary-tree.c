70ad580cab2470d09fd8c4387ddb24bbf354cb8c
lyng148 <113703761+lyng148@users.noreply.github.com>
2023-10-21 13:14:31 +0700

    Time: 0 ms (100.00%) | Memory: 7 MB (10.45%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.c
+++ 226-invert-binary-tree/invert-binary-tree.c
@@ -9,11 +9,13 @@
 void DFS(struct TreeNode* root)
 {
     if (root == NULL) return;
+    DFS(root->left);
+    DFS(root->right);
     struct TreeNode* tmp = root->left;
     root->left = root->right;
     root->right = tmp;
-    DFS(root->left);
-    DFS(root->right);
+    
+    
 }
 struct TreeNode* invertTree(struct TreeNode* root){
     DFS(root);
