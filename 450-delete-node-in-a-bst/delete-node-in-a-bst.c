521a8cc95ce7a9e9ec83e3cf1993a7ea2ee3bd0f
lyng148 <113703761+lyng148@users.noreply.github.com>
2023-10-22 03:13:03 +0700

    Time: 15 ms (83.25%) | Memory: 14.7 MB (12.04%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.c
@@ -0,0 +1,31 @@
+struct TreeNode* findMin(struct TreeNode* node) {
+    while (node->left != NULL) {
+        node = node->left;
+    }
+    return node;
+}
+
+struct TreeNode* deleteNode(struct TreeNode* root, int key) {
+    if (root == NULL) return root;
+
+    if (key < root->val) {
+        root->left = deleteNode(root->left, key);
+    } else if (key > root->val) {
+        root->right = deleteNode(root->right, key);
+    } else {
+        if (root->left == NULL) {
+            struct TreeNode* temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            struct TreeNode* temp = root->left;
+            free(root);
+            return temp;
+        }
+        
+        struct TreeNode* temp = findMin(root->right);
+        root->val = temp->val;
+        root->right = deleteNode(root->right, temp->val);
+    }
+    return root;
+}
