9a5561e15e94f9c008376b1a8ba797da27da1e62
lyng148 <113703761+lyng148@users.noreply.github.com>
2023-10-24 13:57:27 +0700

    Time: 23 ms (7.37%) | Memory: 14.3 MB (61.05%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.c
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.c
@@ -1,31 +1,47 @@
-struct TreeNode* findMin(struct TreeNode* node) {
-    while (node->left != NULL) {
-        node = node->left;
-    }
-    return node;
-}
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
 
-struct TreeNode* deleteNode(struct TreeNode* root, int key) {
-    if (root == NULL) return root;
-
-    if (key < root->val) {
-        root->left = deleteNode(root->left, key);
-    } else if (key > root->val) {
-        root->right = deleteNode(root->right, key);
-    } else {
-        if (root->left == NULL) {
-            struct TreeNode* temp = root->right;
-            free(root);
-            return temp;
-        } else if (root->right == NULL) {
-            struct TreeNode* temp = root->left;
-            free(root);
-            return temp;
+struct TreeNode* deleteNode(struct TreeNode* root, int key){
+    if (!root) return root;
+    if (root)
+    {
+        if (root->val < key)
+            root->right = deleteNode(root->right, key);
+        else if (root->val > key) 
+            root->left = deleteNode(root->left, key);
+        /* o day, chung ta co 4 case:
+        - Case 1: node can xoa la nut la => tra ve NULL de truc tiep xoa node
+        - Case 2: node can xoa co node con ben trai va khong co node con ben phai => tra ve node con ben phai
+        - Case 3: nguoc lai Case 2
+        - Case 4; node can xoa co ca node con trai va phai
+        => ta phai tim duoc node con thay the vao de dam bao thu tu theo inorder
+        => ta se di tim node con lon nhat ben trai hoac node con nho nhat ben phai de the cho vao*/
+        else
+        {
+        // case 1:
+        if (!root->left && !root->right)
+            return NULL;
+        // case 2:
+        if (!root->left && root->right)
+        {
+            return root->right;
         }
-        
-        struct TreeNode* temp = findMin(root->right);
-        root->val = temp->val;
-        root->right = deleteNode(root->right, temp->val);
-    }
+        // case 3:
+        if (!root->right && root->left)
+        {
+            return root->left;
+        }
+        // case 4:
+        struct TreeNode* tmp = root->right;
+        while (tmp->left) tmp = tmp->left;
+        root->val = tmp->val;
+        root->right = deleteNode(root->right, root->val);
+    }}
     return root;
 }
