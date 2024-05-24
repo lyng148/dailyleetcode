8bf4236c2b1a71ee27596988f5976b535b942b5e
lyng148 <113703761+lyng148@users.noreply.github.com>
2024-05-25 00:39:27 +0700

    Time: 1149 ms (20.37%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,9 +1,3 @@
 SELECT w1.id
-FROM Weather w1
-WHERE EXISTS
-(
-    SELECT 1
-    FROM Weather w2
-    WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1
-      AND w1.temperature > w2.temperature
-);
+FROM Weather w1, Weather w2
+WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
