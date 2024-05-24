f3e6f55e7add41ae234489e86d83d12d99c36eea
lyng148 <113703761+lyng148@users.noreply.github.com>
2024-05-25 00:31:20 +0700

    Time: 572 ms (98.80%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,9 @@
+SELECT w1.id
+FROM Weather w1
+WHERE EXISTS
+(
+    SELECT 1
+    FROM Weather w2
+    WHERE w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+      AND w1.temperature > w2.temperature
+);
