b788dd834d4972aeff4dc52d459405452f1b91d5
lyng148 <113703761+lyng148@users.noreply.github.com>
2024-07-24 17:11:08 +0700

    Time: 8 ms (20.53%) | Memory: 20 MB (12.11%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,29 +11,41 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        // Tạo một nút giả (dummy) để dễ dàng xử lý đầu danh sách
+        // Tạo một nút giả để dễ dàng thao tác với đầu của danh sách kết quả
         ListNode dummy;
         ListNode* iterator = &dummy;
-        
+
         // Vòng lặp để gộp hai danh sách theo thứ tự tăng dần
         while (list1 != nullptr && list2 != nullptr) {
             if (list1->val <= list2->val) {
-                iterator->next = list1;
+                // Tạo một nút mới với giá trị của nút hiện tại từ list1
+                ListNode* newNode = new ListNode(list1->val);
+                iterator->next = newNode;
                 list1 = list1->next;
             } else {
-                iterator->next = list2;
+                // Tạo một nút mới với giá trị của nút hiện tại từ list2
+                ListNode* newNode = new ListNode(list2->val);
+                iterator->next = newNode;
                 list2 = list2->next;
             }
             iterator = iterator->next;
         }
-        
+
         // Gắn phần còn lại của danh sách chưa duyệt hết vào cuối danh sách kết quả
-        if (list1 != nullptr) {
-            iterator->next = list1;
-        } else {
-            iterator->next = list2;
+        while (list1 != nullptr) {
+            ListNode* newNode = new ListNode(list1->val);
+            iterator->next = newNode;
+            list1 = list1->next;
+            iterator = iterator->next;
         }
-        
+
+        while (list2 != nullptr) {
+            ListNode* newNode = new ListNode(list2->val);
+            iterator->next = newNode;
+            list2 = list2->next;
+            iterator = iterator->next;
+        }
+
         // Trả về danh sách kết quả, bỏ qua nút giả
         return dummy.next;
     }
