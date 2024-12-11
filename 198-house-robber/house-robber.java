4ddeee942ae907b22cbc2a6eb99e191efc2cbe76
Nguyen Linh (リン) <113703761+lyng148@users.noreply.github.com>
2024-12-12 00:50:35 +0700

    Time: 0 ms (100.00%) | Memory: 41.1 MB (42.48%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,14 +1,21 @@
 class Solution {
     /* the order is: prev2, prev1, num  */
+int[] memo;
 public int rob(int[] nums) {
-    if (nums.length == 0) return 0;
-    int prev1 = 0;
-    int prev2 = 0;
-    for (int num : nums) {
-        int tmp = prev1;
-        prev1 = Math.max(prev2 + num, prev1);
-        prev2 = tmp;
+    memo = new int[nums.length + 1];
+    Arrays.fill(memo, -1);
+    return rob(nums, nums.length - 1);
+}
+
+private int rob(int[] nums, int i) {
+    if (i < 0) {
+        return 0;
     }
-    return prev1;
+    if (memo[i] >= 0) {
+        return memo[i];
+    }
+    int result = Math.max(rob(nums, i - 2) + nums[i], rob(nums, i - 1));
+    memo[i] = result;
+    return result;
 }
 }
