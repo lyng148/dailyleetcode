291fae408e8fe498d191039883f823388f38d817
Nguyen Linh (リン) <113703761+lyng148@users.noreply.github.com>
2024-12-26 19:21:43 +0700

    Time: 803 ms (19.32%) | Memory: 11.6 MB (74.02%) - LeetSync

---
+++ 494-target-sum/target-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int res = 0;
+
+    void tryy(int index, vector<int>& nums, int target) {
+        if (index == nums.size()) {
+            if (target == 0) {
+                res++;
+            }
+        } else {
+            tryy(index + 1, nums, target - nums[index]);
+            tryy(index + 1, nums, target + nums[index]);
+        }
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        tryy(0, nums, target);
+        return res;
+    }
+};
