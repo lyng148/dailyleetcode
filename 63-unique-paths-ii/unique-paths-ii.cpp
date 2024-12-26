8d127a20194f126871c0185dc6c34b46056865a1
Nguyen Linh (リン) <113703761+lyng148@users.noreply.github.com>
2024-12-26 12:27:11 +0700

    Time: 0 ms (100.00%) | Memory: 11.4 MB (46.15%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& nums) {
+        int n = nums.size();
+        int m = nums[0].size();
+
+        if (n == 1 && m == 1){
+            return !nums[n - 1][m - 1];
+        }
+        int dp[n + 1][m + 1];
+        memset(dp, 0, sizeof(dp));
+        dp[0][0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (nums[i][j] != 1) {
+                    if (i > 0) {
+                        if (nums[i - 1][j] != 1)
+                            dp[i][j] += dp[i - 1][j];
+                    }
+                    if (j > 0) {
+                        if (nums[i][j - 1] != 1)
+                            dp[i][j] += dp[i][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+};
