7cc0a0334604b2beb5bdbc3f57781865d8227f3f
Nguyen Linh (リン) <113703761+lyng148@users.noreply.github.com>
2025-08-12 04:43:46 +0700

    Time: 31 ms (27.53%) | Memory: 40.6 MB (12.18%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-  vector<vector<int>> res;
-  set<int> set1, set2;
-  set<int> res1, res2;
+        unordered_set<int> set1(nums1.begin(), nums1.end());
+        unordered_set<int> set2(nums2.begin(), nums2.end());
+        unordered_set<int> res1, res2;
 
-  for (int i = 0; i < nums1.size(); i++){
-    set1.insert(nums1[i]);
-  }        
-  for (int i = 0; i < nums2.size(); i++){
-    set2.insert(nums2[i]);
-    if (!set1.count(nums2[i])) {
-      res1.insert(nums2[i]);
+        for (int n : nums2)
+            if (!set1.count(n)) res1.insert(n);
+
+        for (int n : nums1)
+            if (!set2.count(n)) res2.insert(n);
+
+        vector<int> fres1(res1.begin(), res1.end());
+        vector<int> fres2(res2.begin(), res2.end());
+
+        return {fres2, fres1};
     }
-  }
-  for (int i = 0; i < nums1.size(); i++){
-    if (!set2.count(nums1[i])) {
-      res2.insert(nums1[i]);
-    }
-  }
-  vector<int> fres1(res1.begin(), res1.end());
-  vector<int> fres2(res2.begin(), res2.end());
-  res.push_back(fres2);
-  res.push_back(fres1);
-  return res;
-}
 };
